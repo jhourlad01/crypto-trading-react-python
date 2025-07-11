8c58bafdd1219867a4f7f9963224bb6963eabbef
Joe Estrella <jhourlad01@gmail.com>
2025-07-11 16:23:58 +0800

    Fix npm install problems

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,13 @@
+/* eslint-env node */
+import 'dotenv/config'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:8000'
+/* global process */
+if (!process.env.VITE_API_URL) {
+  throw new Error('VITE_API_URL is not defined in .env');
+}
+const API_URL = process.env.VITE_API_URL;
 
 // https://vite.dev/config/
 export default defineConfig({
