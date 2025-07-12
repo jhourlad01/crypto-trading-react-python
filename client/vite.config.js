1ad803c742fb6a9622b4418e2971b271aaff4a57
Joe Estrella <jhourlad01@gmail.com>
2025-07-12 15:22:36 +0800

    Fix dev environment

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 /* global process */
+console.log('CLIENT_HOST:', process.env.CLIENT_HOST)
+console.log('CLIENT_PORT:', process.env.CLIENT_PORT)
+console.log('VITE_API_URL:', process.env.VITE_API_URL)
+
 if (!process.env.VITE_API_URL) {
   throw new Error('VITE_API_URL is not defined in .env');
 }
@@ -13,6 +17,8 @@ const API_URL = process.env.VITE_API_URL;
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: process.env.CLIENT_HOST || 'localhost',
+    port: parseInt(process.env.CLIENT_PORT) || 3000,
     proxy: {
       '/coins': API_URL
     }
